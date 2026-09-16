110be4938648b6372ada5cff850dd4985abf8879
Ravi <ravivish968@gmail.com>
2026-09-16 14:18:07 +0530

    add searchin2dmatrix2

---
+++ leetcode/searchin2dmatrix2.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class searchin2dmatrix2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+}
