18c01ca0bf790d39411db2695dbd857e1fa9a7c7
Ravi <ravivish968@gmail.com>
2026-09-16 12:56:37 +0530

    add stringCompression

---
+++ leetcode/stringCompression.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class stringCompression {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = chars[i];
+            int count = 0;
+
+            while (i < n && chars[i] == ch ) {
+                count++; i++;   
+            }
+
+            if (count == 1) {
+                chars[idx++] = ch;
+            } else {
+                chars[idx++] = ch;
+                String str = Integer.toString(count);
+                for (int j = 0; j < str.length(); j++) {
+                    chars[idx++] = str.charAt(j);
+                }
+            }
+            i--;
+
+        }
+        return idx;
+    }
+
+
+    public static void main(String[] args) {
+        stringCompression sc = new stringCompression();
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c', 'c'};
+        System.out.println(sc.compress(chars));
+    }
+}
